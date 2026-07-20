7153ed68c4b675edad671f8834ca4e27c7692d70
Ramiq Waqas <ramiqwaqas@gmail.com>
2026-07-20 16:10:55 +0500

    removed scrap code for testing ISP issue

--- test-fetch.js
+++
@@ -1,6 +0,0 @@
-const url = "https://piqskprlebpawgypifbr.supabase.co/rest/v1/";
-console.log("Testing connection to: " + url);
-
-fetch(url)
-  .then(res => console.log("Status:", res.status))
-  .catch(err => console.error("Fetch Error:", err.message, err.cause));
--- test-https.js
+++
@@ -1,9 +0,0 @@
-const https = require('https');
-
-const url = "https://piqskprlebpawgypifbr.supabase.co/rest/v1/";
-
-https.get(url, (res) => {
-  console.log("Status code:", res.statusCode);
-}).on('error', (e) => {
-  console.error("HTTPS Error:", e);
-});
